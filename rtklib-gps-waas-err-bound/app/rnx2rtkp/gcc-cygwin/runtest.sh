f0aebf214bc74d7c441edd64fc9afdf1b9ea382b
jauton <jauton@houghtonassociates.com>
2014-07-28 12:29:59 -0700

    Ignore white space in comparisons with expected results in tests.

--- rtklib-gps-waas-err-bound/app/rnx2rtkp/gcc-cygwin/runtest.sh
+++ rtklib-gps-waas-err-bound/app/rnx2rtkp/gcc-cygwin/runtest.sh
@@ -3,6 +3,6 @@
 declare -i status=0;
 #echo "command: $1" 1>&2
 ${1};
-diff ${2}.pos-expected ${2}.pos > /dev/null
+diff -w ${2}.pos-expected ${2}.pos > /dev/null
 status=$?
 if [ $status -ne 0 ]; then echo "${2} failed, status = $status"; else echo "${2} passed."; fi 1>&2
